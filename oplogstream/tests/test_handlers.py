e43e6d94b1f9c0a674fe43eb8c899af2d26461ea
Sergey Yashchenko <yashenko.s@gmail.com>
2015-10-01 15:37:54 +0300

    Add Queue test

--- oplogstream/tests/test_handlers.py
+++ oplogstream/tests/test_handlers.py
@@ -1,16 +1,20 @@
 import unittest
 
-from oplogstream import handlers
+from oplogstream.handlers import *
 
 class HandlerTest(unittest.TestCase):
 
     def test_base_handler(self):
         with self.assertRaises(NotImplementedError):
-            handlers.OpHandler().handle(None)
+            OpHandler().handle(None)
 
     def test_print_handler(self):
-        handler = handlers.PrintOpHandler()
-        self.assertIsInstance(handler, handlers.OpHandler)
+        handler = PrintOpHandler()
+        self.assertIsInstance(handler, OpHandler)
+
+    def test_queue_handler(self):
+        handler = QueueHandler()
+        self.assertIsInstance(handler, OpHandler)
 
 if __name__ == '__main__':
     unittest.main()
